d17a28a336069d8774b906883d317707b7b68f6a
Ajithpr2001 <101083110+Ajithpr2001@users.noreply.github.com>
2025-02-09 11:55:50 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Menu from "../components/Menu";
 import { TabView, TabPanel } from "primereact/tabview";
 import { Badge } from "primereact/badge";
